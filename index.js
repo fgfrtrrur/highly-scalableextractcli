884783d2ee1474d1229837196c288e50b3e0f903
fgfrtrrur <csjcdanielsousa@hotmail.com>
2024-05-04 17:51:35 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const bubbleSortOptimized = (arr) => {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-        swapped = true;
-      }
+function findSubstring(s, words) {
+  if (s.length === 0 || words.length === 0) return [];
+  const wordMap = new Map();
+  for (const word of words) {
+    wordMap.set(word, (wordMap.get(word) || 0) + 1);
+  }
+  const wordLength = words[0].length;
+  const totalLength = words.length * wordLength;
+  const result = [];
+  for (let i = 0; i <= s.length - totalLength; i++) {
+    const substring = s.substring(i, i + totalLength);
+    if (isValid(substring, new Map(wordMap))) {
+      result.push(i);
     }
-  } while (swapped);
-  return arr;
-};
+  }
+  return result;
+  function isValid(substring, wordMap) {
+    const wordCount = new Map();
+    for (let i = 0; i < substring.length; i += wordLength) {
+      const word = substring.substring(i, i + wordLength);
+      wordCount.set(word, (wordCount.get(word) || 0) + 1);
+    }
+    for (const [key, value] of wordMap) {
+      if ((wordCount.get(key) || 0) !== value) return false;
+    }
+    return true;
+  }
+}
