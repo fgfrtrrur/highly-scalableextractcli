6f6ac35835ac6cf313cd46af6b79686c3e8235bd
fgfrtrrur <csjcdanielsousa@hotmail.com>
2024-05-06 11:13:52 +0700

    demo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function factorial(n) {
-  if (n === 0 || n === 1) return 1;
-  return n * factorial(n - 1);
+function capitalizeWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
