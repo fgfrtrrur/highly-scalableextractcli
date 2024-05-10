4f5a4eef6039abe5140f6e77c46ca9282184eca5
fgfrtrrur <csjcdanielsousa@hotmail.com>
2024-05-11 04:39:03 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-function exist(board, word) {
-  const rows = board.length;
-  const cols = board[0].length;
-  const dfs = (i, j, index) => {
-    if (index === word.length) return true;
-    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] !== word[index])
-      return false;
-    const temp = board[i][j];
-    board[i][j] = "";
-    const found =
-      dfs(i + 1, j, index + 1) ||
-      dfs(i - 1, j, index + 1) ||
-      dfs(i, j + 1, index + 1) ||
-      dfs(i, j - 1, index + 1);
-    board[i][j] = temp;
-    return found;
-  };
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (board[i][j] === word[0] && dfs(i, j, 0)) return true;
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(amount + 1);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
     }
   }
-  return false;
+  return dp[amount] > amount ? -1 : dp[amount];
 }
